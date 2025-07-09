b9bf72620fed3d8cd24299036eb61f7d2e484cb9
peterbrogowski <peterbrogowski@icloud.com>
2025-07-08 20:13:59 -0400

    fix: clean up patched files and finalize app config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
-diff --git a//dev/null b/vite.config.ts
-index 0000000000000000000000000000000000000000..80da60c0fa70ab77a92fc330a3371adca36dcb73 100644
---- a//dev/null
-+++ b/vite.config.ts
-@@ -0,0 +1,9 @@
-+import { defineConfig } from 'vite';
-+import react from '@vitejs/plugin-react';
-+
-+export default defineConfig({
-+  plugins: [react()],
-+  test: {
-+    environment: 'jsdom'
-+  }
-+});
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom'
+  }
+});
