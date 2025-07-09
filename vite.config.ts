08fdef6e047803e09dfe6ad3a237283549e58d4f
peterbrogowski <peterbrogowski@icloud.com>
2025-07-08 21:40:06 -0400

    feat: add index.html and update Vite/Tailwind config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: "public",
   plugins: [react()],
   test: {
     environment: 'jsdom'
   }
+    environment: "jsdom",
+  },
 });
