5662c4ef211054ba49636afd7ca45a4f2d9e041a
peterbrogowski <peterbrogowski@icloud.com>
2025-07-14 22:57:07 -0400

    fix: remove patch markers and restore src

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: "public",
   plugins: [react()],
   test: {
-    environment: 'jsdom'
-  }
+    environment: "jsdom",
+  },
 });
